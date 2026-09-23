5359dccc159b5a1bad87316523dc7f939a06aff4
dushsam <dushsam100@gmail.com>
2026-09-23 22:08:25 +0200

    feat: wire Header and PostList into App

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Header from './components/Header'
+import PostList from './components/PostList'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <PostList />
+      </main>
+    </>
+  )
+}
+
+export default App
